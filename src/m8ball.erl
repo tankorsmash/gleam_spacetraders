fe752b9013d6470e5aebfce70c5a5f4baa690015
tankorsmash <tankorsmash@gmail.com>
2024-03-10 00:59:30 -0500

    cant figure out why its saying start() isnt defined

--- src/m8ball.erl
+++ src/m8ball.erl
@@ -7,9 +7,10 @@
 %%% CALLBACKS %%%
 %%%%%%%%%%%%%%%%%
 
+%% start({failover, Node}, Args) is only called
+%% when a start_phase key is defined.
 start(normal, []) ->
     m8ball_sup:start_link();
-
 start({takeover, _OtherNode}, []) ->
     m8ball_sup:start_link().
 
